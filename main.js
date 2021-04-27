b248eb3ba2cac579dcd1ac7838e6ade1e5dd56ec
std145288 <72416794+std145288@users.noreply.github.com>
2021-04-27 19:14:47 +0300

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ let panelID = "my-info-panel";
  */
 function init() {
   // Create a new Leaflet map centered on the continental US
-  map = L.map("map").setView([24.01, 35.52], 14);
+  map = L.map("map").setView([35.52, 24.01], 14);
 
   // This is the Carto Positron basemap
   L.tileLayer(
