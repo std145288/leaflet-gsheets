89688d76d162f04413926c3a26c5f5f34d5e7af6
std145288 <72416794+std145288@users.noreply.github.com>
2021-04-28 09:02:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ let panelID = "my-info-panel";
  */
 function init() {
   // Create a new Leaflet map centered on Chania Crete
-  map = L.map("map").setView([35.5, 24.0], 12);
+  map = L.map("map").setView([35.5, 24.0], 14);
 
   // This is the Carto Positron basemap
   L.tileLayer(
